344ebdfd4f7dd9a81ff985e7c2f59179db15756e
vitaly <dudkin231@mail.ru>
2024-01-31 10:59:30 +0300

    add some changes

--- app/player/schemas.py
+++ app/player/schemas.py
@@ -5,7 +5,7 @@ class SPlayer(BaseModel):
     name: str = Field(max_length=100, min_length=1)
     NTRP: float = Field(gt=0, lt=10)
     tg_id: int = Field(gt=0)
-    user_name_tg: str = Field(max_length=100, min_length=1)
+    tg_username: str = Field(max_length=100, min_length=1)
     games_played_on_week: int = Field(default=0)
     is_notification: bool = Field(default=False)
     is_notification_changes: bool = Field(default=False)
