15e820eaec07805e93df1aa54ddd747abfcfc585
vitaly <dudkin231@mail.ru>
2024-01-31 10:59:04 +0300

    add some changes

--- app/player/models.py
+++ app/player/models.py
@@ -8,9 +8,9 @@ class Players(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
-    NTRP: Mapped[float]
+    NTRP: Mapped[str] # changes
     tg_id: Mapped[int] = mapped_column(unique=True)
-    user_name_tg: Mapped[str] = mapped_column(unique=True)
+    tg_username: Mapped[str] = mapped_column(unique=True)
     games_played_on_week: Mapped[int] = mapped_column(default=0)
     is_notification: Mapped[bool] = mapped_column(default=False)
     is_notification_changes: Mapped[bool] = mapped_column(default=False)
