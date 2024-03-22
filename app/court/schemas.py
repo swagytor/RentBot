7b7711c5dea039aa9be21b7d4460e0cf83aba858
vitaly <dudkin231@mail.ru>
2024-03-22 12:47:10 +0300

    added new_crud

--- app/court/schemas.py
+++ app/court/schemas.py
@@ -3,5 +3,5 @@ from pydantic import BaseModel, Field
 
 class SCourt(BaseModel):
 
-    address: str = Field(max_length=100, min_length=1)
-    name: str = Field(max_length=100, min_length=1)
+    address: str = Field(max_length=100, min_length=1, unique=True)
+    name: str = Field(max_length=100, min_length=1, unique=True)
--- app/player/router.py
+++ app/player/router.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy import insert, delete, select
 from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import status
 
+from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
 from app.database import get_async_session
 from app.player.models import Player
 from app.player.schemas import SPlayer
@@ -13,52 +16,60 @@ router = APIRouter(
 
 
 @router.post("/")
-async def create_player(new_palayer: SPlayer, se: AsyncSession = Depends(get_async_session), ):
-    new_palayer = insert(Player).values(**new_palayer.model_dump())
-    await se.execute(new_palayer)
-    await se.commit()
-    return {"status": "success"}
+async def create_player(new_player: SPlayer, se: AsyncSession = Depends(get_async_session)):
+    try:
+        new_player = insert(Player).values(**new_player.model_dump())
+        await se.execute(new_player)
+        await se.commit()
+
+        return JSONResponse(content={"status": "success"},
+                            status_code=status.HTTP_201_CREATED)
+    except SQLAlchemyError as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @router.delete("/{player_id}")
 async def delete_player(player_id: int, se: AsyncSession = Depends(get_async_session)):
-    query = delete(Player).where(Player.id == player_id)
-    await se.execute(query)
-    await se.commit()
-    return {"status": "success"}
+    try:
+        player = await se.get(Player, player_id)
+        if player is None:
+            return JSONResponse(content={"error": "Player not found"}, status_code=status.HTTP_404_NOT_FOUND)
+
+        query = delete(Player).where(Player.id == player_id)
+        await se.execute(query)
+        await se.commit()
+
+        return JSONResponse(content={"status": "success"}, status_code=status.HTTP_200_OK)
+    except SQLAlchemyError as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 @router.get("/")
 async def get_all_players(se: AsyncSession = Depends(get_async_session)):
-    query = select(Player)
-    result = await se.execute(query)
-    players = result.scalars().all()
-    return players
+    try:
+        query = select(Player)
+        result = await se.execute(query)
+        players = result.scalars().all()
+        return players
+    except SQLAlchemyError as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-@router.get("/player_id")
+@router.get("/{player_id}")
 async def get_player(player_id: int, se: AsyncSession = Depends(get_async_session)):
-    query = select(Player).filter(Player.id == player_id)
-    result = await se.execute(query)
-    player = result.scalars().first()
-    if player:
-        return player
-    else:
-        return "error:" "Player not found"
+    try:
+        query = select(Player).filter(Player.id == player_id)
+        result = await se.execute(query)
+        player = result.scalars().first()
 
-# @router.get("/{player_id}", response_model=SPlayer)
-# async def get_player(player_id: int, se: AsyncSession = Depends(get_async_session)):
-#     query = select(Player).where(Player.id == player_id)
-#     result = await se.execute(query)
-#     player = await result.scalar()
-#     if player is None:
-#         return 'Not found'
-#     return player
+        if player:
+            return player
+        else:
+            return JSONResponse(content={"error": "Player not found"}, status_code=status.HTTP_404_NOT_FOUND)
 
-# @router.post("/")
-# async def add_specific_product(new_product: ProductCreate, session: AsyncSession = Depends(get_async_session),
-#                                user: User = Depends(current_user)):
-#     stmt = insert(product).values(**new_product.model_dump())
-#     await session.execute(stmt)
-#     await session.commit()
-#     return {"status": "success"}
+    except SQLAlchemyError as e:
+        return JSONResponse(content={"error": f"An error occurred: {str(e)}"},
+                            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
