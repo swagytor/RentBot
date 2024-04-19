06770f95249edceee194d05c2583928e629356c3
Alexey <swagytor@yandex.ru>
2024-04-19 19:43:45 +0300

    Добавление модели Event

--- events/models.py
+++ events/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Event(models.Model):
+    start_date = models.DateTimeField(verbose_name="Начало")
+    end_date = models.DateTimeField(verbose_name="Конец")
+    court = models.ForeignKey('courts.Court', on_delete=models.CASCADE, verbose_name="Корт")
+    player = models.ForeignKey('players.Player', on_delete=models.CASCADE, verbose_name="Игрок")
+
+    def __str__(self):
+        return f"{self.player} {self.start_date}"
+
+    class Meta:
+        verbose_name = "Игра"
+        verbose_name_plural = "Игры"
