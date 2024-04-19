505d335e6580c38057bd636cabf1e32fc8c1d920
Alexey <swagytor@yandex.ru>
2024-04-19 19:43:37 +0300

    Добавление модели Court

--- courts/models.py
+++ courts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Court(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название")
+    address = models.CharField(max_length=256, verbose_name="Адрес", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Корт"
+        verbose_name_plural = "Корты"
