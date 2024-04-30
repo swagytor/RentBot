67cd06201efbf5672cc258084107923023d8a831
vitaly <dudkin231@mail.ru>
2024-04-30 15:43:42 +0300

    added ordering

--- events/views.py
+++ events/views.py
@@ -4,11 +4,15 @@ from events.serializers import EventSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.utils import timezone
+from rest_framework import filters
 
 
 class EventViewSet(viewsets.ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+    filter_backends = [filters.OrderingFilter]
+
+    ordering_fields = ['start_date', 'end_date']
 
     def list(self, request, *args, **kwargs):
         court = self.request.query_params.get('court')
