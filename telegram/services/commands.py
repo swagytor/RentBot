5873cc854bfb9a31c0bbcf7d3dba1ca9881c97ca
vitaly <dudkin231@mail.ru>
2024-05-15 10:10:42 +0300

    fast fix buttons start

--- telegram/services/commands.py
+++ telegram/services/commands.py
@@ -5,7 +5,7 @@ from aiogram.types import BotCommand
 async def set_commands(bot: Bot):
     commands = [
         BotCommand(
-            command='start',
+            command='starts',
             description='Запустить бота'
         ),
         BotCommand(
